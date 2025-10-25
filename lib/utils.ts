4130c09f9c29d9dab331a022fdb5741674a22beb
360 Magicians <360magicians@gmail.com>
2025-10-25 10:28:58 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,11 @@
+def encode_gid(type_name: str, db_id: str) -> str:
+    return f"gid://pinksync/{type_name}/{db_id}"
+
+def decode_gid(gid: str):
+    # Example: gid://pinksync/Provider/123
+    _, _, type_name, db_id = gid.split('/')
+    return "pinksync", type_name, db_id
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
